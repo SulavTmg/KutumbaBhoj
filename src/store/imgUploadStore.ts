e1e7ff55f472d616c1f75d021cb35c622b2f1a0a
Sulav Tamang <syangtansulav@gamil.com>
2024-09-25 09:12:07 +0545

    Update: Major cleanup and refactor

--- src/store/imgUploadStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import { ImgStore } from "../types/imgUploads";
-
-const imgUploadStore = create<ImgStore>(() => ({
-  imgs: [],
-}));
-
-export default imgUploadStore;
--- src/store/menuStore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { MenuStore } from "../types/menu";
-
-const menuStore = create<MenuStore>(() => ({
-  menu: null,
-  item: null,
-}));
-
-export default menuStore;
